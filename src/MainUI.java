bd3ffec1f6a1022acec25adfa77ab1fcde86e1b4
Malan <malandealwis@gmail.com>
2024-12-27 09:31:38 +0530

    Add main UI options for menu navigation

--- src/MainUI.java
+++ src/MainUI.java
@@ -1,5 +1,60 @@
+import java.util.Scanner;
 public class MainUI {
     public static void main(String[] args) {
+        MainUI UI = new MainUI();
+        Scanner input =new Scanner(System.in);
 
+        System.out.println("---Welcome to BusTikka---");
+        UI.loginProcess();
+        int inputNumber = input.nextInt();
+
+        switch (inputNumber) {
+            case 1:
+               // break;
+            case 2:
+               // break;
+            default:
+                System.out.println("Invalid Input");
+
+        }
+
+        UI.showMainMenu();
+        int inputMenuNumber = input.nextInt();
+
+        switch (inputMenuNumber) {
+            case 1:
+                // break;
+            case 2:
+                // break;
+            case 3:
+                // break;
+            case 4:
+                // break;
+            case 5:
+                // break;
+            case 6:
+                break;
+            default:
+                System.out.println("Invalid Input");
+
+        }
+
+
+
+    }
+
+    private void showMainMenu() {
+        System.out.println("---Select Option---");
+        System.out.println("        1) Calculate Distance");
+        System.out.println("        2) Calculate Bus Ticket");
+        System.out.println("        3) Book a Ticket");
+        System.out.println("        4) Search a Ticket");
+        System.out.println("        5) Cancel a Ticket");
+        System.out.println("        6) Quit");
+    }
+
+    private  void loginProcess(){
+        System.out.println("1) Register");
+        System.out.println("2) Login");
     }
 }
